29627c55454624de8edefc18c4ab8e81d6f04fe2
Daniil Popov <x6uhrox@gmail.com>
2023-06-26 12:02:53 +0300

    FIX: Header for mobile and tablet

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,32 +21,48 @@ const Header = () => {
             </Link>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/" className="fs-3">
-              Home
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/visitors" className="fs-3">
-              Visitors
-            </Nav.Link>
-          </Nav>
           <Navbar.Collapse
             id="basic-navbar-nav"
-            className="justify-content-end"
+            className="justify-content-between"
           >
-            {isLoggedIn && (
-              <span className="text-white me-4 fs-5">Hello, {username}!</span>
-            )}
+            <Nav className="me-auto mb-2 mb-lg-0">
+              <Nav.Link as={NavLink} to="/" className="fs-3">
+                Home
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/visitors" className="fs-3">
+                Visitors
+              </Nav.Link>
+            </Nav>
             <Nav>
-              <Link to="/login" className="btn btn-primary me-3">
-                Log In
-              </Link>
-              <Link
-                to="/logout"
-                className="btn btn-primary"
-                onClick={() => dispatch(authOperation.logOut())}
-              >
-                Log Out
-              </Link>
+              {isLoggedIn ? (
+                <span className="text-white me-4 fs-5">Hello, {username}!</span>
+              ) : (
+                <>
+                  <Link
+                    to="/login"
+                    className="btn btn-primary me-3 d-none d-lg-inline-block"
+                  >
+                    Log In
+                  </Link>
+                  <Link
+                    to="/logout"
+                    className="btn btn-primary d-none d-lg-inline-block"
+                    onClick={() => dispatch(authOperation.logOut())}
+                  >
+                    Log Out
+                  </Link>
+                  <Link to="/login" className="nav-link fs-5 d-lg-none">
+                    Log In
+                  </Link>
+                  <Link
+                    to="/logout"
+                    className="nav-link fs-5 d-lg-none"
+                    onClick={() => dispatch(authOperation.logOut())}
+                  >
+                    Log Out
+                  </Link>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
